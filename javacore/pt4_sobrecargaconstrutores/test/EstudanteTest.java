e9949eda01a8fd8901fa1b4755c2cbc67579fc73
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-06-15 13:54:42 -0300

    Parte 4 - Sobrecarga de Construtores

---
+++ javacore/pt4_sobrecargaconstrutores/test/EstudanteTest.java
@@ -0,0 +1,10 @@
+package maratonajava.javacore.pt4_sobrecargaconstrutores.test;
+
+import maratonajava.javacore.pt4_sobrecargaconstrutores.classes.Estudante;
+
+public class EstudanteTest {
+    public static void main(String[] args) {
+        Estudante est = new Estudante("12212", "Carlitos", new double[] {5,7,9}, "20/10/1997");
+        est.imprime();
+    }
+}
