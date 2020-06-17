6312069d7ce92adf3d54d7d99bc2809d6fc7f8ca
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-06-17 20:19:00 -0300

    Parte 8 - Herança

---
+++ javacore/pt8_heranca/classes/Endereco.java
@@ -0,0 +1,25 @@
+package maratonajava.javacore.pt8_heranca.classes;
+
+public class Endereco {
+    private String rua;
+    private String bairro;
+
+    public Endereco() {
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+}
