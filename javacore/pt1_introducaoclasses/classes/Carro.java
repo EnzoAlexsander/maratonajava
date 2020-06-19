12f66fdfa99eead64f2b9af1d7dcb7435838ce2b
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-06-19 14:56:09 -0300

    Renomeando Parte 1

--- javacore/pt1_introducaoclasses/classes/Carro.java
+++
@@ -1,7 +0,0 @@
-package maratonajava.javacore.pt1_introducaoclasses.classes;
-
-public class Carro {
-    public String placa;
-    public String modelo;
-    public float velocidadeMaxima;
-}
--- javacore/pt1_introducaoclasses/classes/Estudante.java
+++
@@ -1,7 +0,0 @@
-package maratonajava.javacore.pt1_introducaoclasses.classes;
-
-public class Estudante {
-    public String nome;
-    public String matricula;
-    public int idade;
-}
