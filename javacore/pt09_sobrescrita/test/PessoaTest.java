3dd80d723c64631ff55764a04a92ba091b8b3e9f
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-06-24 11:18:35 -0300

    Parte 9 - Sobrescrita

---
+++ javacore/pt09_sobrescrita/test/PessoaTest.java
@@ -0,0 +1,17 @@
+package maratonajava.javacore.pt09_sobrescrita.test;
+
+import maratonajava.javacore.pt09_sobrescrita.classes.Pessoa;
+
+public class PessoaTest {
+    public static void main(String[] args) {
+        Pessoa p = new Pessoa();
+        p.setNome("Paul Nedved");
+        p.setIdade(47);
+        System.out.println(p);
+        System.out.println("-----------------------");
+        Pessoa p2 = new Pessoa();
+        p2.setNome("Dennis Bergkamp");
+        p2.setIdade(51);
+        System.out.println(p2);
+    }
+}
