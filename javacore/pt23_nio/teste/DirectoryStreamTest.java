351375013af256b1005d2ca096c050a19da2b41b
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-07-03 14:52:46 -0300

    Parte 23 - NIO

---
+++ javacore/pt23_nio/teste/DirectoryStreamTest.java
@@ -0,0 +1,20 @@
+package maratonajava.javacore.pt23_nio.teste;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class DirectoryStreamTest {
+    public static void main(String[] args) {
+        Path dir = Paths.get("pasta");
+        try(DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+            for(Path path : stream) {
+                System.out.println(path.getFileName());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
