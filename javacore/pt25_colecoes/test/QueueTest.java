85d6b57725a1d9e5391db20a2d945950ccf3180f
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-09-22 13:56:04 -0300

    Parte 25 - Coleções

---
+++ javacore/pt25_colecoes/test/QueueTest.java
@@ -0,0 +1,17 @@
+package maratonajava.javacore.pt25_colecoes.test;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueTest {
+    public static void main(String[] args) {
+
+        Queue<String> queue = new PriorityQueue<>();
+        queue.add("D");
+        queue.add("A");
+        queue.add("B");
+        System.out.println(queue.peek());
+        System.out.println(queue.size());
+        System.out.println(queue.poll());
+    }
+}
